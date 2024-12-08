a0c690f097f8e7cc120d9ea08db92477a3ac5198
mxdz-ux <cmadrideo.9217@umak.edu.ph>
2024-12-08 22:49:29 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,18 @@
-const swiper = new Swiper (".slider-container" , {
+const swiper = new Swiper(".slider-container", {
     effect: "slide",
     speed: 1300,
-    //autoplay: {delay: 4000}
+    //autoplay: {delay: 4000},
     navigation: {
-        prevEl: "#slide-prev" ,
+        prevEl: "#slide-prev",
         nextEl: "#slide-next"
-    } ,
-    });
+    }
+});
+
+window.addEventListener('scroll', function() {
+    const navbar = document.querySelector('.navbar');
+    if (window.scrollY > 50) {
+        navbar.classList.add('scrolled');
+    } else {
+        navbar.classList.remove('scrolled');
+    }
+});
