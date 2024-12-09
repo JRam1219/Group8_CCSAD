8851407dc2eea12a19b48d7f80acddefb5a1ad01
mxdz-ux <cmadrideo.9217@umak.edu.ph>
2024-12-09 23:42:12 +0800

    Update script.js

--- script.js
+++ script.js
@@ -9,18 +9,14 @@ const swiper = new Swiper(".slider-container", {
 });
 
 document.addEventListener('DOMContentLoaded', function() {
-    const navbar = document.querySelector('.navbar.fixed-top');
+    const navbar = document.querySelector('.navbar');
     
     if (navbar) {
         window.addEventListener('scroll', function() {
             if (window.scrollY > 50) {
                 navbar.classList.add('scrolled');
-                navbar.style.backgroundColor = 'rgba(35, 36, 35, 0.8)';
-                navbar.style.boxShadow = '0 5px 10px rgba(245, 245, 245, 0.315)';
             } else {
                 navbar.classList.remove('scrolled');
-                navbar.style.backgroundColor = 'transparent';
-                navbar.style.boxShadow = 'none';
             }
         });
     }
