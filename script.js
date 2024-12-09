0cf0dc54687da9281444fd3644a0e1bb981076e9
mxdz-ux <cmadrideo.9217@umak.edu.ph>
2024-12-10 00:47:04 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,12 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const swiper = new Swiper(".slider-container", {
-        effect: "slide",
-        speed: 1300,
-        // Optional autoplay
-        // autoplay: {delay: 4000},
-        navigation: {
-            prevEl: "#slide-prev",
-            nextEl: "#slide-next"
-        }
+const swiper = new Swiper (".slider-container" , {
+    effect: "slide",
+    speed: 1300,
+    //autoplay: {delay: 4000}
+    navigation: {
+        prevEl: "#slide-prev" ,
+        nextEl: "#slide-next"
+    } ,
     });
-});
 document.addEventListener('DOMContentLoaded', () => {
     const navbar = document.querySelector('.navbar.navbar-expand-lg.fixed-top');
     if (navbar) {
