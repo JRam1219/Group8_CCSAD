bfee26b1692b5364863659602920b9b08cf7d2cc
mxdz-ux <cmadrideo.9217@umak.edu.ph>
2024-12-09 23:22:54 +0800

    Update script.js

--- script.js
+++ script.js
@@ -8,15 +8,6 @@ const swiper = new Swiper(".slider-container", {
     }
 });
 
-window.addEventListener('scroll', function() {
-    const navbar = document.querySelector('.navbar');
-    if (window.scrollY > 50) {
-        navbar.classList.add('scrolled');
-    } else {
-        navbar.classList.remove('scrolled');
-    }
-});
-
 document.addEventListener('DOMContentLoaded', function() {
     const navbar = document.querySelector('.navbar.fixed-top');
     
