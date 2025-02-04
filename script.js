e4ac8f705775a635e3b4651064d2359e86dd4e96
mxdz-ux <cmadrideo.9217@umak.edu.ph>
2025-02-04 19:18:02 +0800

    Update script.js

--- script.js
+++ script.js
@@ -2,10 +2,6 @@ const swiper = new Swiper (".slider-container" , {
     effect: "slide",
     speed: 1300,
     autoplay: {delay: 4000}
-    navigation: {
-        prevEl: "#slide-prev" ,
-        nextEl: "#slide-next"
-    } ,
 });
 
 window.addEventListener('scroll', function() {
